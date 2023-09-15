78308f5bbba6d14a445c4be61451628b8cd96fa0
sexnine <25860388+sexnine@users.noreply.github.com>
2023-09-15 18:09:22 +1000

    Add types and consts

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def ShipLogic(round: int, yourMap, enemyMap, enemyHp, yourHp, yourShotSeq, yourPrevHit: bool):
+from typing import List
+
+SHIP_SIZES = [5, 3, 3, 2, 2]
+BOARD_SIZE = 10
+
+def ShipLogic(round: int, yourMap, enemyMap, enemyHp: int, yourHp: int, yourShotSeq: List[(int, int)], yourPrevHit: bool) -> (int, int):
     pass
