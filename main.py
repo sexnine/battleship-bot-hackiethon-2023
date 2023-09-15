3f2c295293e556c5d66438b69ca64ed047be8963
sexnine <25860388+sexnine@users.noreply.github.com>
2023-09-15 21:41:50 +1000

    add state, change function signature

--- main.py
+++ main.py
@@ -3,5 +3,36 @@ from typing import List
 SHIP_SIZES = [5, 3, 3, 2, 2]
 BOARD_SIZE = 10
 
-def ShipLogic(round: int, yourMap, enemyMap, enemyHp: int, yourHp: int, yourShotSeq: List[(int, int)], yourPrevHit: bool) -> (int, int):
-    pass
+
+class GameState:
+    def __init__(self):
+        self.board = Board()
+
+
+class Board:
+    def __init__(self):
+        self.board = [[BoardCell() for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+
+
+class BoardCell:
+    def __init__(self):
+        self.checked = False
+        self.hit = False
+
+    def set_checked(self):
+        self.checked = True
+
+    def set_hit(self):
+        self.checked = True
+        self.hit = True
+
+
+def ShipLogic(round: int, map, enemy_hp: int, hp: int, shot_sequence: List[(int, int)],
+              did_previous_shot_hit: bool, storage: List) -> (int, int):
+    game_state = None
+
+    if len(storage) > 0:
+        game_state = storage[0]
+    else:
+        game_state = GameState()
+        storage.append(game_state)
